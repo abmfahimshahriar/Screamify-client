5c1f5e07e4b9b71de657c99a74f52fc5d4a339ec
abmfahimshahriar <abmfahimshahriar@iut-dhaka.edu>
2020-12-14 05:37:42 +0600

    add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
-
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 // pages
 import home from "./pages/home";
 import login from "./pages/login";
@@ -9,9 +10,27 @@ import signup from "./pages/signup";
 // components
 import NavBar from "./components/Navbar";
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#33c9dc',
+      main: '#00bcd4',
+      dark: '#008394',
+      contrastText: '#fff'
+    },
+    secondary: {
+      light: '#ff6333',
+      main: '#ff3d00',
+      dark: '#b22a00',
+      contrastText: '#fff'
+    }
+  },
+})
+
 function App() {
   return (
     <div className="App">
+      <MuiThemeProvider theme={theme}>
       <Router>
         <NavBar />
         <div className="container">
@@ -22,6 +41,8 @@ function App() {
           </Switch>
         </div>
       </Router>
+      </MuiThemeProvider>
+      
     </div>
   );
 }
