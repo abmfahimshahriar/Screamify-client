991dc8fab39d435e8fa6abfb48a22a8033751a4a
abmfahimshahriar <abmfahimshahriar@iut-dhaka.edu>
2020-12-14 04:14:42 +0600

    add initial pages

---
+++ src/pages/signup.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class signup extends React.Component {
+    render() {
+      return (
+        <div>
+          <h1>signup page</h1>
+        </div>
+      );
+    }
+  }
+
+export default signup
+
