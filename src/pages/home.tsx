515864ad35b06b3a3a14d4c17e452cac70dc3bf6
abmfahimshahriar <abmfahimshahriar@iut-dhaka.edu>
2020-12-14 06:18:26 +0600

    fetch screams

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,14 +1,47 @@
-import React from 'react'
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import axios from "axios";
 
-class home extends React.Component {
-    render() {
-      return (
-        <div>
-          <h1>home page</h1>
-        </div>
-      );
-    }
+import {Scream} from './models/home';
+
+interface State {
+  screams: Scream[]
+}
+class home extends React.Component<State> {
+  state: State = {
+    screams: [],
+  };
+  componentDidMount() {
+    axios
+      .get("/screams")
+      .then((res) => {
+        this.setState({
+          screams: res.data,
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   }
+  render() {
+    let recentScreamsMarkup = this.state.screams ? (
+      this.state.screams.map((scream) => (
+        <p key={scream.screamId}>{scream.body}</p>
+      ))
+    ) : (
+      <p>Loading...</p>
+    );
+    return (
+      <Grid container spacing={2}>
+        <Grid item sm={8} xs={12}>
+          {recentScreamsMarkup}
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <p>Profile....</p>
+        </Grid>
+      </Grid>
+    );
+  }
+}
 
-export default home
-
+export default home;
---
+++ src/pages/models/home.tsx
@@ -0,0 +1,9 @@
+export interface Scream {
+    body: string;
+    commentCount: number;
+    createdAt: string;
+    likeCount: number;
+    screamId: string;
+    userHandle: string;
+    userImage: string;
+}
