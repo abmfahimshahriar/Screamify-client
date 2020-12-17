d0993f82a578742b31d7eac8ffa8223941d7d760
abmfahimshahriar <abmfahimshahriar@iut-dhaka.edu>
2020-12-17 10:12:31 +0600

    implement data actions

--- src/redux/actions/dataActions.tsx
+++ src/redux/actions/dataActions.tsx
@@ -1,2 +1,52 @@
+import {
+  SET_SCREAMS,
+  LOADING_DATA,
+  LIKE_SCREAM,
+  UNLIKE_SCREAM,
+} from "../types";
 import axios from "axios";
-import { SET_USER, SET_ERRORS, CLEAR_ERRORS, LOADING_UI } from "../types";
+
+// get all screams
+export const getScreams = () => (dispatch: any) => {
+  dispatch({ type: LOADING_DATA });
+
+  axios
+    .get("/screams")
+    .then((res) => {
+      dispatch({
+        type: SET_SCREAMS,
+        payload: res.data,
+      });
+    })
+    .catch((err) => {
+      dispatch({
+        type: SET_SCREAMS,
+        payload: [],
+      });
+    });
+};
+
+// Like a scream
+export const likeScream = (screamId: string) => (dispatch: any) => {
+  axios
+    .get(`/scream/${screamId}/like`)
+    .then((res) => {
+      dispatch({
+        type: LIKE_SCREAM,
+        payload: res.data,
+      });
+    })
+    .catch((err) => console.log(err));
+};
+// Unlike a scream
+export const unlikeScream = (screamId: string) => (dispatch: any) => {
+    axios
+      .get(`/scream/${screamId}/unlike`)
+      .then((res) => {
+        dispatch({
+          type: UNLIKE_SCREAM,
+          payload: res.data,
+        });
+      })
+      .catch((err) => console.log(err));
+  };
